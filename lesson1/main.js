b49f0914f91729caefa163fbcfd3d9450a953417
A_STRO <abulovshoxruh8@gmail.com>
2022-02-25 21:54:53 +0500

    masala yozdim js da

---
+++ lesson1/main.js
@@ -0,0 +1,18 @@
+ console.log("Siz kiritgan son 27");
+ console.log("1.13 49");
+ console.log("7.16   100");
+ console.log("1 2 3 4");
+ console.log("5")
+ console.log("10")
+ console.log("21")
+ console.log("1 2");
+ console.log("3 4");
+ console.log("5 10");
+ console.log("7sm");
+ console.log(14 ** 5);
+ console.log(2 ** 4);
+ console.log(2 ** 6);
+ console.log(((12-6)*2)**3);
+let x = 5 
+let y = 7 * x + 3 * x + 6 
+console.log("y=" + y);
