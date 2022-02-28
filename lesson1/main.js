dc4c775585bcdde78529117ee98c1c5647de01aa
A_STRO <abulovshoxruh8@gmail.com>
2022-02-28 21:27:59 +0500

    masalalar ishladik

--- lesson1/main.js
+++ lesson1/main.js
@@ -1,18 +1,59 @@
- console.log("Siz kiritgan son 27");
- console.log("1.13 49");
- console.log("7.16   100");
- console.log("1 2 3 4");
- console.log("5")
- console.log("10")
- console.log("21")
- console.log("1 2");
- console.log("3 4");
- console.log("5 10");
- console.log("7sm");
- console.log(14 ** 5);
- console.log(2 ** 4);
- console.log(2 ** 6);
- console.log(((12-6)*2)**3);
-let x = 5 
-let y = 7 * x + 3 * x + 6 
-console.log("y=" + y);
+//  console.log("Siz kiritgan son 27");
+//  console.log("1.13 49");
+//  console.log("7.16   100");
+//  console.log("1 2 3 4");
+//  console.log("5")
+//  console.log("10")
+//  console.log("21")
+//  console.log("1 2");
+//  console.log("3 4");
+//  console.log("5 10");
+//  console.log("7sm");
+//  console.log(14 ** 5);
+//  console.log(2 ** 4);
+//  console.log(2 ** 6);
+//  console.log(((12-6)*2)**3);
+// let x = 5 
+// let y = 7 * x + 3 * x + 6 
+// console.log("y=" + y);
+let a = 5 
+console.log("a=" + 4*a);
+let r = 50
+console.log(r*2);
+let pi = 3.14
+r = 50
+let s =  pi * r ** 2
+console.log(s);
+a = 5 
+let b = 6 
+let A = (a+b)/2
+console.log(A);
+S = (a*b)**1/2
+console.log(S);
+a = 6
+b = 8
+console.log("p="+2*(a+b));
+console.log(a*b);
+a = 100 
+b = 300  
+let S1 = + a * b
+a = 10 
+b = 25 
+let S2 = + a * b
+console.log(S1 / S2);
+let m = 11000
+let x = 0.5
+console.log(m * x);
+a = 11000
+b = 15000
+let c = 25000
+x = prompt ('Olma nechta kg olmokchisiz')
+let y = prompt ('pechi nechi kg olmokchisiz')
+let z = prompt ('koft nechi kg olmokchisiz')
+let ax = a * x
+let by = b * y 
+let cz = c * z
+console.log("olma" , ax);
+console.log("pechi" , by);
+console.log("koft" , cz);
+console.log("hamasi" , ax + by + cz);
